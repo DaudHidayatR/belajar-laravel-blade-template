cb1994a2396fce77649fce0402e7b300018af6da
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-04-05 14:40:51 +0700

    Belajar For Loop

---
+++ resources/views/forelse.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+    @forelse($hobbies as $hobby)
+        <li>{{@$hobby}}</li>
+    @empty
+        <li>Tidak punya hobby</li>
+    @endforelse
+
+</ul>
+</body>
+</html>
