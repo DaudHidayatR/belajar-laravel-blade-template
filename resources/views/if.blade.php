34a04f99cffa9a508356378960e2a717e96b8b6c
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-04-05 13:30:34 +0700

    Belajar  If Statement

---
+++ resources/views/if.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>
+    @if(count($hobbies) == 1)
+        I have one hobby!
+    @elseif(count($hobbies)>1)
+        I have multiple hobbies!
+    @else
+        I don't have any hobbies!
+    @endif
+</p>
+</body>
+</html>
